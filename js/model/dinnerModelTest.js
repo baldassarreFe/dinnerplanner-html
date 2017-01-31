9d0450fb0e2caeca98bba6e01295b313086dd494
Federico Baldassarre <baldassarre.fe@gmail.com>
2017-01-31 15:25:37 +0100

    Model business logic done

---
+++ js/model/dinnerModelTest.js
@@ -0,0 +1,14 @@
+var dm = new DinnerModel();
+
+dm.getNumberOfGuests();
+
+dm.addDishToMenu(101);
+dm.addDishToMenu(203);
+
+dm.getAllIngredients();
+dm.getTotalMenuPrice();
+
+dm.setNumberOfGuests(2); 
+
+dm.getAllIngredients();
+dm.getTotalMenuPrice();
